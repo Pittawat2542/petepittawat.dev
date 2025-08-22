607cf3978aa767cbe8eae2954646d1f9e245a460
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2025-08-23 00:25:59 +0700

    chore: clean up and improve metadata

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,25 @@
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
-
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection('blog');
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.excerpt,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.slug}/`,
+      categories: post.data.tags || [],
+      // Enrich feed with full content (Markdown/MDX body)
+      content: post.body,
+    })),
+    // Add namespace + extra data
+    customData:
+      '<language>en</language>\n' +
+      '<xmlns:content="http://purl.org/rss/1.0/modules/content/"/>',
+  });
 }
